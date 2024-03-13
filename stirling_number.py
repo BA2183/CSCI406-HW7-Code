0be2ab7619c488a9438e3d0a2c07e4c056ef6935
dcnguyen <dcnguyen@mines.edu>
2024-03-13 16:28:28 -0600

    remove tempt py
    py is kinda dog water when visualizing table

--- stirling_number.py
+++
@@ -1,9 +0,0 @@
-# def tp_dp(n, k):
-    
-# def bu_dp(n, k):
-
-def main(n, k):
-    memory = [[0 for col in range(max(n, k)+1)] for row in range(max(n, k)+1)]
-    return memory
-
-print(main(7, 4))
